5c5c8e7094a7694cb3ca51390f6f079960081f1e
Makoto Shiozumi <shiozumi@esmile-hd.com>
2021-02-17 10:37:12 +0900

    2d

--- config/rebase_016.sh
+++ config/rebase_016.sh
@@ -1,5 +1,10 @@
 #/bin/sh
 
+#
+# まずは、ローカルのリポジトリー作成から！
+#
+git init
+
 echo "# rebase" > README.md
 
 git add README.md
@@ -16,36 +21,30 @@ git remote add origin https://github.com/user-name/rebase.git
 git push -u origin main
 
 #
-#
-#
-echo "# rebase" > README.md
-git add README.md
-git commit -m "1st"
-
-#
-#
+# 2nd コミット
 #
 echo '# AAA' >> README.md
 git README.md
 git commit -m 'A'
 
 #
-#
+# 3rd コミット
 #
 echo '# BBB' >> README.md
 git README.md
 git commit -m 'B'
 
 #
-#
+# 4th コミット
 #
 echo '# CCC' >> README.md
 git README.md
 git commit -m 'C'
 
 #
-#
+# 現在状態を確認
 #
 git log -p
+
 
 
